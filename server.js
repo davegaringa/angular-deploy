f5d919e7a96cc8822e191313ec9c650a94b0af1c
David II Garinga <davegaringa@hotmail.com>
2019-12-19 16:44:03 +0800

    Updated server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/angular-deploy-demo1'));
+app.use(express.static(__dirname + '/dist/angular-deploy'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-deploy-demo1/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
 });
 
 // default Heroku PORT
